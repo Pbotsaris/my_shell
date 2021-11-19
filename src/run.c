0b1474f99d5a11e47f5cdadc86217d7909da49c8
Pbotsaris <pedro@antfood.com>
2021-11-19 09:22:11 +0100

    removed tests from run

--- src/run.c
+++ src/run.c
@@ -28,83 +28,9 @@ void main_loop(char **envs)
     program->print_prompt(program);
     program->readline(program);
 
-    token_t *token = program->lexer->get_next_token(program->lexer);
-
-    if(token != NULL && token->type == EXIT)
-      printf("was exit!\n");
-
-    if(token != NULL && token->type == ECHO)
-    {
-      printf("was echo!\n");
-      free(token);
-
-      token = program->lexer->get_next_token(program->lexer);
-      printf("type: %d, value: %s\n", token->type, token->value);
-      free(token->value);
-      free(token);
-
-
-      token = program->lexer->get_next_token(program->lexer);
-      printf("type: %d, value: %s\n", token->type, token->value);
-      free(token);
-
-      token = program->lexer->get_next_token(program->lexer);
-      printf("type: %d, value: %s\n", token->type, token->value);
-      free(token->value);
-      free(token);
-
-      token = program->lexer->get_next_token(program->lexer);
-      printf("type: %d, value: %s\n", token->type, token->value);
-    }
-
-    if(token != NULL && token->type == ENV)
-      printf("was ENV!\n");
-
-    if(token != NULL && token->type == SETENV)
-      printf("was setenv!\n");
-
-    if(token != NULL && token->type == UNSETENV)
-      printf("was unsetenv!\n");
-
-    if(token != NULL && token->type == WHICH)
-      printf("was was which!\n");
-
-    if(token != NULL && token->type == PWD)
-      printf("was was pwd!\n");
-
-    if(token != NULL && token->type == VARIABLE_ASSIGN)
-    {
-      printf("was VAR ASSIGN -> %s!\n", token->value);
-      free(token->value);
-      free(token);
-
-      token = program->lexer->get_next_token(program->lexer);
-
-      printf("type: %d, value: %s\n", token->type, token->value);
-      free(token);
-
-      token = program->lexer->get_next_token(program->lexer);
-      printf("type: %d, value: %s\n", token->type, token->value);
-      free(token->value);
-
-    }
-
-    if(token != NULL && token->type == PASS_THROUGH)
-    {
-      printf("was WAS PASS THROUGH:!\n");
-      free(token->value);
-    }
-
-
-
-
-    if(token)
-      free(token);
+    program->free(program);
 
   }
-
-  program->free(program);
-
 }
 
 
